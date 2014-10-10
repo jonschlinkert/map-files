cbaec0d9218fde23405d320cc25f3e3ab4b97004
jonschlinkert <dev@sellside.com>
2014-10-10 01:28:15 -0400

    minor refactoring

--- index.js
+++ index.js
@@ -8,83 +8,40 @@ var fs = require('fs');
 var path = require('path');
 var glob = require('globby');
 
-
-/**
- * Default renaming function, returns the
- * basename of the file.
- *
- * ```js
- * rename('foo/bar/baz.txt')
- * //=> 'baz'
- * ```
- *
- * @param  {String} `filepath`
- * @param  {Object} `opts`
- * @return {String}
- * @api private
- */
-
-function rename(filepath, opts) {
-  if (opts.rename) {
-    return opts.rename(filepath, opts);
-  }
-  return filepath;
-}
-
-
-/**
- * Default parsing function. By default this function returns an object
- * with the following properties:
- *
- *   - `name` {String}: the basename of the file, excluding extension
- *   - `contents` {String}: the file's contents
- *
- * ```js
- * parse('foo/bar/baz.txt')
- * //=> {path: "foo/bar/baz.txt", contents: "This is the file's contents!"}
- * ```
- *
- * @param  {String} `filepath`
- * @param  {Object} `opts`
- * @return {String}
- * @api private
- */
-
-function parse(filepath, opts) {
-  if (opts.parse) {
-    return opts.parse(filepath, opts);
-  }
-  var name = path.basename(filepath, path.extname(filepath));
-  var str = fs.readFileSync(filepath, 'utf8');
-  return {name: name, contents: str};
-}
-
-
 /**
  * Return an object for all files matching the given `patterns`
  * and `options`. The full filepath of the file is used as the key.
  *
  * @param  {String} `patterns` Glob patterns to pass to [globby]
- * @param  {Object} `opts` Options for globby, or pass a custom `parse` or `rename`.
+ * @param  {Object} `opts` Options for globby, or pass a custom `parse` or `name`.
  * @return {Object}
  * @api public
  */
 
-function mapFiles(patterns, opts) {
-  var files = glob.sync(patterns, opts);
-  opts = opts || {};
+module.exports = function mapFiles(patterns, options) {
+  var files = glob.sync(patterns, options);
 
-  return files.reduce(function (acc, filepath) {
-    var name = rename(filepath, opts);
-    var file = parse(filepath, opts);
-    acc[name] = file;
-    return acc;
+  return files.reduce(function (cache, filepath) {
+    var key = name(filepath, options);
+    var str = read(filepath, options);
+
+    cache[key] = str;
+    return cache;
   }, {});
+};
+
+function name(filepath, options) {
+  if (options && options.name) {
+    return options.name(filepath, options);
+  }
+  var ext = path.extname(filepath);
+  return path.basename(filepath, ext);
 }
 
-
-/**
- * Expose `mapFiles`
- */
-
-module.exports = mapFiles;
+function read(filepath, options) {
+  if (options && options.read) {
+    return options.read(filepath, options);
+  }
+  var str = fs.readFileSync(filepath, 'utf8');
+  return {path: filepath, content: str};
+}
