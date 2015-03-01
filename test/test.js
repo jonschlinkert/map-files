297db863406025cc8629a84203319256c8eddc5c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-01 03:55:50 -0500

    update

--- test/test.js
+++ test/test.js
@@ -49,95 +49,94 @@ describe('files', function () {
     });
   });
 
-  // it('should load files from a glob pattern.', function () {
-  //   var cache = files('test/fixtures/*.txt');
+  it('should load files from a glob pattern.', function () {
+    var cache = files('test/fixtures/*.txt');
 
-  //   cache.should.have.property('a');
-  //   cache.should.have.property('b');
-  //   cache.should.have.property('c');
-  //   cache.should.eql({
-  //     a: { content: 'AAA', path: 'test/fixtures/a.txt' },
-  //     b: { content: 'BBB', path: 'test/fixtures/b.txt' },
-  //     c: { content: 'CCC', path: 'test/fixtures/c.txt' }
-  //   });
-  // });
+    cache.should.have.property('a');
+    cache.should.have.property('b');
+    cache.should.have.property('c');
+    cache.should.eql({
+      a: { content: 'AAA', path: 'test/fixtures/a.txt' },
+      b: { content: 'BBB', path: 'test/fixtures/b.txt' },
+      c: { content: 'CCC', path: 'test/fixtures/c.txt' }
+    });
+  });
 
-  // it('should use a cwd.', function () {
-  //   var cache = files('*.txt', {cwd: 'test/fixtures'});
+  it('should use a cwd.', function () {
+    var cache = files('*.txt', {cwd: 'test/fixtures'});
 
-  //   cache.should.have.property('a');
-  //   cache.should.have.property('b');
-  //   cache.should.have.property('c');
-  //   cache.should.eql({
-  //     a: { content: 'AAA', path: 'test/fixtures/a.txt' },
-  //     b: { content: 'BBB', path: 'test/fixtures/b.txt' },
-  //     c: { content: 'CCC', path: 'test/fixtures/c.txt' }
-  //   });
-  // });
+    cache.should.have.property('a');
+    cache.should.have.property('b');
+    cache.should.have.property('c');
+    cache.should.eql({
+      a: { content: 'AAA', path: 'test/fixtures/a.txt' },
+      b: { content: 'BBB', path: 'test/fixtures/b.txt' },
+      c: { content: 'CCC', path: 'test/fixtures/c.txt' }
+    });
+  });
 
-  // it('should rename the key with a custom function.', function () {
-  //   var cache = files('test/fixtures/*.txt', {
-  //     name: function(filepath) {
-  //       return relative(filepath);
-  //     }
-  //   })
-  //   cache.should.have.property('test/fixtures/a.txt');
-  //   cache.should.have.property('test/fixtures/b.txt');
-  //   cache.should.have.property('test/fixtures/c.txt');
-  // });
+  it('should rename the key with a custom function.', function () {
+    var cache = files('test/fixtures/*.txt', {
+      name: function(filepath) {
+        return relative(filepath);
+      }
+    })
+    cache.should.have.property('test/fixtures/a.txt');
+    cache.should.have.property('test/fixtures/b.txt');
+    cache.should.have.property('test/fixtures/c.txt');
+  });
 
-  // it('should read files with a custom function.', function () {
-  //   var cache = files('test/fixtures/*.txt', {
-  //     read: function(filepath) {
-  //       var res = matter.read(filepath);
-  //       res.path = relative(res.path);
-  //       return res;
-  //     }
-  //   });
-  //   cache.should.have.property('a', { data: {}, content: 'AAA', orig: 'AAA', path: 'test/fixtures/a.txt' });
-  //   cache.should.have.property('b', { data: {}, content: 'BBB', orig: 'BBB', path: 'test/fixtures/b.txt' });
-  //   cache.should.have.property('c', { data: {}, content: 'CCC', orig: 'CCC', path: 'test/fixtures/c.txt' });
-  // });
+  it('should read files with a custom function.', function () {
+    var cache = files('test/fixtures/*.txt', {
+      read: function(filepath) {
+        var res = matter.read(filepath);
+        res.path = relative(res.path);
+        return res;
+      }
+    });
+    cache.should.have.property('a', { data: {}, content: 'AAA', orig: 'AAA', path: 'test/fixtures/a.txt' });
+    cache.should.have.property('b', { data: {}, content: 'BBB', orig: 'BBB', path: 'test/fixtures/b.txt' });
+    cache.should.have.property('c', { data: {}, content: 'CCC', orig: 'CCC', path: 'test/fixtures/c.txt' });
+  });
 
-  // it('should require files with a custom function.', function () {
-  //   var cache = files('test/fixtures/*.js', {
-  //     read: function (filepath) {
-  //       return {
-  //         path: relative(filepath),
-  //         helper: require(path.resolve(filepath))
-  //       }
-  //     }
-  //   });
-  //   cache.should.have.property('a');
-  //   cache.should.have.property('b');
-  //   cache.should.have.property('c');
-  //   cache['a'].path.should.equal('test/fixtures/a.js');
-  //   cache['a'].helper.should.be.an.object;
-  //   cache['a'].helper.should.be.a.function;
-  // });
+  it('should require files with a custom function.', function () {
+    var cache = files('test/fixtures/*.js', {
+      read: function (filepath) {
+        return {
+          path: relative(filepath),
+          helper: require(path.resolve(filepath))
+        }
+      }
+    });
+    cache.should.have.property('a');
+    cache.should.have.property('b');
+    cache.should.have.property('c');
+    cache['a'].path.should.equal('test/fixtures/a.js');
+    cache['a'].helper.should.be.an.object;
+    cache['a'].helper.should.be.a.function;
+  });
 
-  // it('should use multiple custom functions.', function () {
-  //   var cache = files('test/fixtures/*.txt', {
-  //     read: function(filepath) {
-  //       var res = matter.read(filepath);
-  //       res.path = relative(res.path);
-  //       return res;
-  //     },
-  //     name: function(filepath) {
-  //       return relative(filepath);
-  //     }
-  //   });
-  //   cache.should.have.property('test/fixtures/a.txt', { data: {}, content: 'AAA', orig: 'AAA', path: 'test/fixtures/a.txt' });
-  //   cache.should.have.property('test/fixtures/b.txt', { data: {}, content: 'BBB', orig: 'BBB', path: 'test/fixtures/b.txt' });
-  //   cache.should.have.property('test/fixtures/c.txt', { data: {}, content: 'CCC', orig: 'CCC', path: 'test/fixtures/c.txt' });
-  // });
+  it('should use multiple custom functions.', function () {
+    var cache = files('test/fixtures/*.txt', {
+      read: function(filepath) {
+        var res = matter.read(filepath);
+        res.path = relative(res.path);
+        return res;
+      },
+      name: function(filepath) {
+        return relative(filepath);
+      }
+    });
+    cache.should.have.property('test/fixtures/a.txt', { data: {}, content: 'AAA', orig: 'AAA', path: 'test/fixtures/a.txt' });
+    cache.should.have.property('test/fixtures/b.txt', { data: {}, content: 'BBB', orig: 'BBB', path: 'test/fixtures/b.txt' });
+    cache.should.have.property('test/fixtures/c.txt', { data: {}, content: 'CCC', orig: 'CCC', path: 'test/fixtures/c.txt' });
+  });
 
-  // it('readme example #2.', function () {
-  //   var cache = files('test/fixtures/*.js', {
-  //     read: function (fp) {
-  //       return require(path.resolve(fp));
-  //     }
-  //   });
-  //   // console.log(cache);
-  // });
+  it('readme example #2.', function () {
+    var cache = files('test/fixtures/*.js', {
+      read: function (fp) {
+        return require(path.resolve(fp));
+      }
+    });
+  });
 });
