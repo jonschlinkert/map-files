5dcbb59f2336bcc9ce70a5fb74fa424a54b2dad5
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-01 03:38:30 -0500

    add caching

--- index.js
+++ index.js
@@ -9,53 +9,108 @@ var path = require('path');
 var globby = require('globby');
 
 /**
+ * Expose `mapFiles`
+ */
+
+module.exports = mapFiles;
+
+/**
  * Return an object for all files matching the given `patterns`
- * and `opts`. The full filepath of the file is used as the key.
+ * and `opts`. By default the full filepath of the file is used
+ * as the key, but this can be changed by passing a `name` function
+ * on the options.
  *
  * @param  {String} `patterns` Glob patterns to pass to [globby]
- * @param  {Object} `opts` Options for globby, or pass a custom `parse` or `name`.
+ * @param  {Object} `opts` Options for globby, or the following:
+ *     @option {Boolean} [opts] `cache` If true, results will be cached in memory so that subsequent lookups for the same cwd and patterns don't repeatedly hit the file system.
+ *     @option {Function} [opts] `glob`: Custom glob function. Default is globby.
+ *     @option {Function} [opts] `read`: Custom read function. Default is `fs.readFileSync`
+ *     @option {Function} [opts] `name`: Custom name function for renaming object keys. Default is the asename of the file, excluding extension.
  * @return {Object}
  * @api public
  */
 
-module.exports = function mapFiles(patterns, opts) {
+function mapFiles(patterns, opts) {
+  opts.cwd = opts && opts.cwd || process.cwd();
   var files = glob(patterns, opts);
-  var cwd = opts && opts.cwd || process.cwd();
 
+  if (opts.cache === true) {
+    var fn = memo('files', opts.cwd, patterns, reduce);
+    return fn(files, opts);
+  }
+
+  return reduce(files, opts);
+};
+
+/**
+ * Expose results cache.
+ */
+
+var cache = mapFiles.cache = {};
+
+function reduce(files, opts) {
   return files.reduce(function (acc, fp) {
-    fp = path.resolve(cwd, fp);
+    fp = path.resolve(opts.cwd, fp);
     var key = name(fp, acc, opts);
     var str = read(fp, acc, opts);
-
     acc[key] = str;
     return acc;
   }, {});
-};
+}
+
+/**
+ * Default glob function
+ */
 
 function glob(patterns, opts) {
   if (opts && opts.glob) {
     return opts.glob(patterns, opts);
   }
+  if (opts.cache === true) {
+    var fn = memo('glob', opts.cwd, patterns, globby.sync);
+    return fn(patterns, opts);
+  }
   return globby.sync(patterns, opts);
 }
 
+/**
+ * Default renaming function
+ */
+
 function name(fp, acc, opts) {
-  if (opts && opts.name) {
+  var fn = opts && opts.name || opts.renameKey;
+  if (fn) {
     return opts.name(fp, acc, opts);
   }
   var ext = path.extname(fp);
   return path.basename(fp, ext);
 }
 
+/**
+ * Default read function
+ */
+
 function read(fp, acc, opts) {
   fp = path.resolve(fp);
-
   if (opts && opts.read) {
     return opts.read(fp, acc, opts);
   }
-
   var res = {};
   res.content = fs.readFileSync(fp, 'utf8');
   res.path = fp;
   return res;
+}
+
+/**
+ * Files cache.
+ */
+
+function memo(name, cwd, patterns, fn) {
+  var key = name + ':' + cwd + '/' + patterns;
+  return function () {
+    if (cache.hasOwnProperty(key)) {
+      return cache[key];
+    }
+    return (cache[key] = fn.apply(fn, arguments));
+  };
 }
